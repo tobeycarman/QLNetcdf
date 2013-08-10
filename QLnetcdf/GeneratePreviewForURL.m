1100a6f757e621e4bdac921b32cd540b1745d5fc
Tobey Carman <tobeycarman@gmail.com>
2013-08-09 22:47:36 -0800

    messing around with looping over all the variables

--- QLnetcdf/GeneratePreviewForURL.m
+++ QLnetcdf/GeneratePreviewForURL.m
@@ -40,14 +40,34 @@ OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview,
                            "  Unlimited Dim: %i\n"
                            " }\n", ncid, ndims, nvars, ngatts, unlimdimid
                            ];
+        NSLog(@"QLnetcdt basic inquiry for %s", cfilepath);
+        NSLog(data);
+        
 
-        for (int i = 0; i <nvars; ++i){
-            
+        for (int curvar_id = 0; curvar_id < nvars; ++curvar_id){
+            char curvar_name[NC_MAX_NAME+1];      /* variable name */
+            nc_type curvar_type;                   /* variable type */
+            int curvar_ndims;                      /* number of dims */
+            int curvar_dimids[NC_MAX_VAR_DIMS];    /* dimension IDs */
+            int curvar_natts;                      /* number of attributes */
+
+            /* we don't need name, since we already know it */
+            status = nc_inq_var(ncid, curvar_id, curvar_name, &curvar_type, &curvar_ndims, curvar_dimids,
+                                 &curvar_natts);
+            if (status != NC_NOERR) handle_error(status);
+            NSString * vardata = [NSString stringWithFormat:@"Variable Name: %s\n"
+                               "  Type: %i\n"
+                               "  Number of Dims: %i\n"
+                               "  Dim id[0]: %i\n"
+                               "  Attributes: %i\n"
+                               " }\n", curvar_name, curvar_type, curvar_ndims, curvar_dimids[1], curvar_natts
+                               ];
+            NSLog(vardata);
         }
         
-        NSLog(@"QLnetcdt basic inquiry for %s", cfilepath);
         
-        NSLog(data);
+        
+
         //NSLog(@"Netcdf (status: %i), (ncid: %i), (ndims: %i), (nvars: %i), (ngatts: %i), (unlimdimid: %i),",
         //      status, ncid, ndims, nvars, ngatts, unlimdimid);
 
